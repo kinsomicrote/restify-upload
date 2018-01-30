c510f713865212aef2a4c1860ffb1373f543b1ed
kinsomicrote <kinsomicrote.inc@gmail.com>
2018-01-30 17:28:23 +0100

    added uuid module for making images unique

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,21 +1,26 @@
 const restify = require('restify')
 const mkdirp = require('mkdirp')
 const fs = require('fs')
+const uuid = require('uuid')
 
-exports.upload = (req, res) => {
+exports.upload = async (req, res) => {
   const file = req.files.image
   const filename = file.name.split('.')
   const extension = filename[filename.length - 1]
-  let upload =  (function (err) {
-    let readImage = fs.createReadStream(file.path)
-    if (err) {
-      console.log(err)
-    }
-    let new_path = `./uploads/${new Date().getTime()}.${extension}`
-    mkdirp('./uploads')
-    let outImage = fs.createWriteStream(new_path)
-    readImage.pipe(outImage)
-  })()
+  try {
+    let upload =  await (function (err) {
+      let readImage = fs.createReadStream(file.path)
+      if (err) {
+        console.log(err)
+      }
+      let new_path = `./uploads/${uuid.v4()}.${extension}`
+      mkdirp('./uploads')
+      let outImage = fs.createWriteStream(new_path)
+      readImage.pipe(outImage)
+    })()
+  } catch(err) {
+    throw err
+  }
 }
 
 exports.validateFile = async (req, res) => {

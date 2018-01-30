2dd184e74cac20a8b86a480b38dbbdf5764c1918
kinsomicrote <kinsomicrote.inc@gmail.com>
2018-01-30 16:55:00 +0100

    file can now be uploaded

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -4,16 +4,14 @@ const fs = require('fs')
 
 exports.upload = (req, res) => {
   const file = req.files.image
-  let filename = file.name
-  let upload =  (function (err, file) {
-    const f = req.files.image
-    let readImage = fs.createReadStream(f.path)
+  const filename = file.name.split('.')
+  const extension = filename[filename.length - 1]
+  let upload =  (function (err) {
+    let readImage = fs.createReadStream(file.path)
     if (err) {
       console.log(err)
-    } else {
-      console.log('done')
     }
-    let new_path = `./uploads/${new Date().getTime()}`
+    let new_path = `./uploads/${new Date().getTime()}.${extension}`
     mkdirp('./uploads')
     let outImage = fs.createWriteStream(new_path)
     readImage.pipe(outImage)
